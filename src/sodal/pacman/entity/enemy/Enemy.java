d05028b9a47ea5f8b6c345567cc78f45ad086843
Asha Sodal <ashasodalswe@hotmail.com>
2025-05-11 15:40:25 +0200

    rect[3] enemy collision done

--- src/sodal/pacman/entity/enemy/Enemy.java
+++ src/sodal/pacman/entity/enemy/Enemy.java
@@ -10,10 +10,11 @@ public class Enemy extends Entity {
     private Rectangle[] rect;
     public Enemy(int x, int y, int width, int height, int speed) {
         super(x, y, width, height, speed,"./src/sodal/pacman/entity/enemy/image/clyde.png");
-        rect = new Rectangle[4];
+        rect = new Rectangle[5];
         rect[0] = new Rectangle(this.x, this.y + 11, this.width, this.height - 11);
         rect[1] = new Rectangle(this.x + 2, this.y + 6, this.width-4, 5);
         rect[2] = new Rectangle(this.x + 3, this.y + 4, this.width-7, 2);
+        rect[3] = new Rectangle(this.x + 5, this.y + 2, this.width-10, 2);
     }
 
 
@@ -34,7 +35,7 @@ public class Enemy extends Entity {
         g2.drawImage(this.image, this.x, this.y,this.width, this.height,null );
 
         // Set a semi-transparent blue color (alpha range: 0.0f - fully transparent to 1.0f - fully opaque)
-        float alpha = 0.8f; // 50% transparent
+        float alpha = 1.0f; // 50% transparent
         g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
         g2.setColor(new Color(123, 0, 255, (int)(255 * alpha))); // Blue with transparency
        g2.fillRect(this.rect[1].x, this.rect[1].y, this.rect[1].width, this.rect[1].height);
@@ -51,6 +52,11 @@ public class Enemy extends Entity {
         g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
         g2.setColor(new Color(25, 20, 250, (int)(255 * alpha))); // Blue with transparency
         g2.fillRect(this.rect[2].x, this.rect[2].y, this.rect[2].width, this.rect[2].height);
+
+
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+        g2.setColor(new Color(250, 20, 250, (int)(255 * alpha))); // Blue with transparency
+        g2.fillRect(this.rect[3].x, this.rect[3].y, this.rect[3].width, this.rect[3].height);
 
 
 // Optional: reset composite to full opacity for other drawings
